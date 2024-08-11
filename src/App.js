2d0932960cbc0ea3c9e146a39a780fcc253b5ec8
bibashu <bibachu8@gmail.com>
2024-08-11 23:15:36 +0000

    mise a jour

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+"use client";
+
+import { Button, Drawer, Sidebar, TextInput } from "flowbite-react";
+import { useState } from "react";
+import {
+  HiChartPie,
+  HiClipboard,
+  HiCollection,
+  HiInformationCircle,
+  HiLogin,
+  HiPencil,
+  HiSearch,
+  HiShoppingBag,
+  HiUsers,
+} from "react-icons/hi";
+
+export default function App() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => setIsOpen(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex min-h-[50vh] items-center justify-center">
+        <Button onClick={() => setIsOpen(true)}>Show navigation</Button>
+      </div>
+      <Drawer open={isOpen} onClose={handleClose}>
+        <Drawer.Header title="MENU" titleIcon={() => <></>} />
+        <Drawer.Items>
+          <Sidebar
+            aria-label="Sidebar with multi-level dropdown example"
+            className="[&>div]:bg-transparent [&>div]:p-0"
+          >
+            <div className="flex h-full flex-col justify-between py-2">
+              <div>
+                <form className="pb-3 md:hidden">
+                  <TextInput icon={HiSearch} type="search" placeholder="Search" required size={32} />
+                </form>
+                <Sidebar.Items>
+                  <Sidebar.ItemGroup>
+                    <Sidebar.Item href="/" icon={HiChartPie}>
+                      Dashboard
+                    </Sidebar.Item>
+                    <Sidebar.Item href="/e-commerce/products" icon={HiShoppingBag}>
+                      Products
+                    </Sidebar.Item>
+                    <Sidebar.Item href="/users/list" icon={HiUsers}>
+                      Users list
+                    </Sidebar.Item>
+                    <Sidebar.Item href="/authentication/sign-in" icon={HiLogin}>
+                      Sign in
+                    </Sidebar.Item>
+                    <Sidebar.Item href="/authentication/sign-up" icon={HiPencil}>
+                      Sign up
+                    </Sidebar.Item>
+                  </Sidebar.ItemGroup>
+                  <Sidebar.ItemGroup>
+                    <Sidebar.Item href="https://github.com/themesberg/flowbite-react/" icon={HiClipboard}>
+                      Docs
+                    </Sidebar.Item>
+                    <Sidebar.Item href="https://flowbite-react.com/" icon={HiCollection}>
+                      Components
+                    </Sidebar.Item>
+                    <Sidebar.Item href="https://github.com/themesberg/flowbite-react/issues" icon={HiInformationCircle}>
+                      Help
+                    </Sidebar.Item>
+                  </Sidebar.ItemGroup>
+                </Sidebar.Items>
+              </div>
+            </div>
+          </Sidebar>
+        </Drawer.Items>
+      </Drawer>
+    </>
   );
 }
-
-export default App;
